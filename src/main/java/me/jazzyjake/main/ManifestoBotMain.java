0e46a9c122d5d7ab4c0a7e7254f46978c5798b3d
jazzy-and-saxy <69865215+jazzy-and-saxy@users.noreply.github.com>
2021-06-01 19:54:21 -0700

    Added status switcher which switches from showing the help command to the website URL every minute

--- src/main/java/me/jazzyjake/main/ManifestoBotMain.java
+++ src/main/java/me/jazzyjake/main/ManifestoBotMain.java
@@ -6,16 +6,20 @@ import me.jazzyjake.listeners.ManifestoListener;
 import me.jazzyjake.listeners.ManifestoReactionListener;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.entities.Activity;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import javax.security.auth.login.LoginException;
-import java.lang.reflect.InvocationTargetException;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.Arrays;
 import java.util.ResourceBundle;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 public class ManifestoBotMain {
     // application.properties constants
@@ -29,19 +33,33 @@ public class ManifestoBotMain {
     public static final String DERBY_PROTOCOL = "jdbc:derby:";
 
     private static JDA jda;
+    private static final ScheduledExecutorService executorService = Executors.newSingleThreadScheduledExecutor();
     private static final Logger log = LogManager.getLogger(ManifestoBotMain.class);
 
     public static void main(String[] args) {
-        log.info("Current manifesto blacklist: {}", Arrays.toString(MANIFESTO_BLACKLIST));
-
         // Creates the JDA bot and builds it
         try {
             jda = JDABuilder
                     .createDefault(TOKEN)
                     .addEventListeners(new ManifestoListener(), new CommandListener(), new ManifestoReactionListener())
                     .build();
-        } catch (LoginException e) {
-            log.error("Login exception occurred!");
+
+            jda.awaitReady();
+
+            log.info("Current manifesto blacklist: {}", Arrays.toString(MANIFESTO_BLACKLIST));
+
+            // Switches bot presence between showing !manifestohelp (help command) and the manifesto website
+            executorService.scheduleAtFixedRate(() -> {
+                if (jda.getPresence().getActivity() == null || jda.getPresence().getActivity().getType() == Activity.ActivityType.WATCHING) {
+                    jda.getPresence().setPresence(Activity.listening("!manifestohelp"), false);
+                } else {
+                    // TODO Change this to actual website
+                    jda.getPresence().setPresence(Activity.watching("jazzyjake.ddns.net"), false);
+                }
+            }, 0, 1, TimeUnit.MINUTES);
+
+        } catch (LoginException | InterruptedException e) {
+            log.error("Startup exception occurred!");
             e.printStackTrace();
         }
     }
